4a256d84b96bf0cba74b0dac1fb5383ca69930a0
crissdev <criss.trifan@gmail.com>
2015-10-13 01:21:08 +0300

    relative links to images

--- app/services/packages.service.js
+++ app/services/packages.service.js
@@ -6,25 +6,25 @@ export function PackagesService() {
       name: 'grunt',
       title: 'Grunt',
       description: 'The JavaScript Task Runner.',
-      logoUrl: '/assets/images/grunt-logo-100.png'
+      logoUrl: 'assets/images/grunt-logo-100.png'
     },
     {
       name: 'gulp',
       title: 'Gulp',
       description: 'The streaming build system.',
-      logoUrl: '/assets/images/gulp-logo-100.png'
+      logoUrl: 'assets/images/gulp-logo-100.png'
     },
     {
       name: 'jspm',
       title: 'JSPM',
       description: 'Registry and format agnostic JavaScript package manager.',
-      logoUrl: '/assets/images/jspm-logo-100.png'
+      logoUrl: 'assets/images/jspm-logo-100.png'
     },
     {
       name: 'yeoman',
       title: 'Yeoman',
       description: `The web's scaffolding tool for modern webapps.`,
-      logoUrl: '/assets/images/yeoman-logo-100.png'
+      logoUrl: 'assets/images/yeoman-logo-100.png'
     }
   ];
 
