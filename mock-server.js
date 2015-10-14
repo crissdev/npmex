4303541f6c5a150166d899090bdbe8f99453b72d
crissdev <criss.trifan@gmail.com>
2015-10-14 15:31:39 +0300

    update mock server

--- mock-server.js
+++ mock-server.js
@@ -6,19 +6,19 @@ var tinyLr = require('tiny-lr')();
 var watch = require('node-watch');
 
 var config = {
-  host: '0.0.0.0',
-  port: 3000
+  web: {port: 4000, host: '0.0.0.0'},
+  lr: {port: 4001, host: '0.0.0.0'}
 };
 
 var app = express();
-app.use(liveReload());
+app.use(liveReload(config.lr));
 app.use(express.static(__dirname));
 
-tinyLr.listen(35729, config.host);
-app.listen(config.port, config.host);
+tinyLr.listen(config.lr.port, config.lr.host);
+app.listen(config.web.port, config.web.host);
 
-watch(['app/', 'assets/', 'index.html', 'config.js'], {recursive: true }, function(filename) {
+watch(['app/', 'assets/', 'index.html', 'config.js'], {recursive: true}, function(filename) {
   tinyLr.changed({body: {files: [filename]}});
 });
 
-console.log(`Server started (http://${config.host}:${config.port})`);
+console.log(`Server started (http://${config.web.host}:${config.web.port})`);
